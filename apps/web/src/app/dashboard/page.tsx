d264713d25718bede04701f1876a31cc4637333a
WetBananas <everlyn.frellita@gmail.com>
2024-02-19 22:11:57 +0700

    docs: added simple dashboard UI

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,7 +1,93 @@
+'use client';
 import React from 'react';
+import { Disclosure } from '@headlessui/react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import {
+  MdSpaceDashboard,
+  MdOutlineEvent,
+  MdOutlineAccountCircle,
+  MdOutlineNotifications,
+  MdOutlineCreditCard,
+  MdOutlineSettings,
+  MdOutlineLogout,
+} from 'react-icons/md';
 
-const Dashboard = () => {
-  return <div>This is Dashboard</div>;
-};
+function SideBar() {
+  return (
+    <div>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group hover:bg-gray-900">
+              <GiHamburgerMenu
+                className={`block md:hidden h-6 w-6 ${
+                  open ? 'transform rotate-90' : ''
+                }`}
+                aria-hidden="true"
+              />
+            </Disclosure.Button>
+            <Disclosure.Panel className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:w-60 lg:left-0 peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+              <div className="flex flex-col justify-start items-center">
+                <h1 className="text-base text-center cursor-pointer font-bold text-purple-900 border-b border-gray-100 pb-4 w-full">
+                  Logo
+                </h1>
+                <div className="my-4-border-b border-gray-100 pb-4">
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Dashboard
+                    </h3>
+                  </div>
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdOutlineAccountCircle className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Profile
+                    </h3>
+                  </div>
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdOutlineNotifications className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Notification
+                    </h3>
+                  </div>
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdOutlineEvent className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Events
+                    </h3>
+                  </div>
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdOutlineCreditCard className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Biling
+                    </h3>
+                  </div>
+                </div>
 
-export default Dashboard;
+                <div className="my-4">
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Settings
+                    </h3>
+                  </div>
+                </div>
+
+                <div className="my-4">
+                  <div className="flex mb-2 justify-start items-center gap-4 px-5 border border-gray-200 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                    <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white" />
+                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                      Logout
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  );
+}
+
+export default SideBar;
